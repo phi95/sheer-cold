7e04753c568265c91c287fd12d2134b87fd91d63
justintumale <fjetumale@yahoo.com>
2016-03-26 05:03:24 -0400

    added separate frames to the user interface

--- SheerCold.py
+++ SheerCold.py
@@ -1,21 +1,40 @@
 #!/usr/bin/python
 import serial #Import Serial Library
-from Tkinter import *
+from tkinter import *
 from time import sleep
 
 root = Tk()
 root.minsize(300,300)
 root.geometry("500x500")
-root.title("Sheer Cold")
 
 var = StringVar()
 degreesText = " F"
-arduinoSerialData = serial.Serial('/dev/cu.usbmodemFD121', 9600) #object...tell it which COMPORT are you on?
+#arduinoSerialData = serial.Serial('/dev/cu.usbmodemFD121', 9600) #object...tell it which COMPORT are you on?
+arduinoSerialData = serial.Serial('/dev/cu.usbmodem641', 9600) #object...tell it which COMPORT are you on?
+
+#creates the top frame
+topFrame = Frame(root)
+topFrame.pack()
+
+#creates the bottom frame
+bottomFrame = Frame(root)
+bottomFrame.pack(side=BOTTOM)
+
+#create label
+label_1 = Label(bottomFrame, text="Set Temperature")
+#create entry form
+entry_1 = Entry(bottomFrame)
+#bind label to the frame
+label_1.grid(row=0, column=0)
+#bind entry form to the frame
+entry_1.grid(row=1, column=0)
 
 def update():
-	while 1:
-		var.set(arduinoSerialData.readline() + degreesText)
-		root.update()
+	while True:
+		line = arduinoSerialData.readline()
+		line = line.decode('utf-8')
+		var.set(line + degreesText)
+		topFrame.update()
 		sleep(1)
 
 #Setting the logo
