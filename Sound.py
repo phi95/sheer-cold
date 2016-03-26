65e4236379b1894406df0e7e3567a30f1ae8536f
Quanzu <Dhn74201@uga.edu>
2016-03-26 05:19:51 -0400

    Alarm
    Add in alarm sound, install simpleaudio via command:
    pip3 install simpleaudio

---
+++ Sound.py
@@ -0,0 +1,9 @@
+import simpleaudio as sa
+
+def playAlarm(double tempCondition):
+
+    wave_obj = sa.WaveObject.from_wave_file("C:/Users/huann_000/Desktop/SheerCold/test.wav")
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
+
+
