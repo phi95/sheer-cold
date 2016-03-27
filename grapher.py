47a0e895a248bf359bd140be927deed4342d374f
justintumale <fjetumale@yahoo.com>
2016-03-26 23:24:16 -0400

    added x and y labels and a title to the graph

--- grapher.py
+++ grapher.py
@@ -3,16 +3,26 @@ import matplotlib.animation as animation
 from matplotlib import style
 
 
+
 style.use('fivethirtyeight')
 
 fig = plt.figure()
-
-
+'''
+plt.xlabel('Time (s)')
+plt.ylabel('Temperature (F)')
+plt.title("Your drink's current temperature")
+'''
 ax1 = fig.add_subplot(1,1,1)
+fig.subplots_adjust(bottom=0.1)
+
+
 
 
 def animate(i):
-    graph_data = open('example.txt', 'r').read()
+    ax1.set_ylabel('what')
+    ax1.set_title('hi')
+
+    graph_data = open('plot.txt', 'r').read()
     lines = graph_data.split('\n')
     xs = []
     ys = []
@@ -22,10 +32,11 @@ def animate(i):
             xs.append(x)
             ys.append(y)
     ax1.clear()
+    ax1.set_ylabel("Temperature (F)")
+    ax1.set_title("Drink Temperatures")
+    ax1.set_xlabel("Time (s)")
     ax1.plot(xs,ys)
 
-ani = animation.FuncAnimation(fig, animate, interval = 1000)
-plt.xlabel('Time (s)')
-plt.ylabel('Temperature (F)')
-plt.title("Your drink's current temperature")
+ani = animation.FuncAnimation(fig, animate,  interval=1000)
+
 plt.show()
